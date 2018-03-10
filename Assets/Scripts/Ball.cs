647c89af38b0ab8c0e752182594949859e70eec5
Jannis Baudisch <jannis.baudisch@nocapes.de>
2018-03-10 20:00:12 +0100

    Music and some stuff

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -39,4 +39,10 @@ public class Ball : MonoBehaviour {
         rigidbody.simulated = true;
         sr.sprite = playerAlive;
     }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Wall") {
+            GameManager.SoundManager.HitWall();
+        }
+    }
 }
